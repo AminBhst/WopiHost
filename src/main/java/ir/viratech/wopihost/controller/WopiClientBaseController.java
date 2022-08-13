eb50b39e95a6d43e57527c4769c169eca7fea915
beheshti <beheshti@saadafzar.com>
2022-08-13 13:48:20 +0430

    write file

--- src/main/java/ir/viratech/wopihost/controller/WopiClientBaseController.java
+++ src/main/java/ir/viratech/wopihost/controller/WopiClientBaseController.java
@@ -1,5 +1,6 @@
 package ir.viratech.wopihost.controller;
 
+import ir.viratech.wopihost.config.ConfigHolder;
 import ir.viratech.wopihost.dto.EntryPointDTO;
 import ir.viratech.wopihost.exception.InvalidFileTypeException;
 import ir.viratech.wopihost.service.generator.WopiUrlGenerator;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.UUID;
 
@@ -33,8 +36,16 @@ public class WopiClientBaseController {
     @PostMapping(value = "/entry-point")
     public EntryPointDTO generateWopiUrl(@RequestPart("file") MultipartFile file) throws IOException, InvalidFileTypeException {
         String uuid = UUID.randomUUID().toString();
-        validateFileType(file.getOriginalFilename());
+        String originalFilename = file.getOriginalFilename();
+        validateFileType(originalFilename);
+        String fileName = uuid + originalFilename.substring(originalFilename.lastIndexOf("."));
+        writeFile(file,fileName);
         return new EntryPointDTO(urlGenerator.generate(uuid));
+    }
+
+    private void writeFile(MultipartFile file,String fileName) throws IOException {
+        String path = ConfigHolder.getConfig().getFileDirectoryPath();
+        Files.copy(file.getInputStream(), Paths.get(path).resolve(fileName));
     }
 
     private void validateFileType(String fileName) throws InvalidFileTypeException {
